57666f7a3dcfeb30821416b902ee960fd778339e
Sang Yi <Sang Yi>
2016-07-03 17:20:29 -0500

    added test for list component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  render: function(){
+      return(
+      <div>
+        AddTodo
+      </div>
+    )
+  }
+});
+
+module.exports = AddTodo;
---
+++ app/components/Search.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Search = React.createClass({
+  render: function(){
+      return(
+      <div>
+        Search
+      </div>
+    )
+  }
+});
+
+module.exports = Search;
