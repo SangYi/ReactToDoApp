e9e2bee76d3e1deed6ea8a7771554f89f8dad3bc
sangyi <songyee1984@yahoo.com>
2016-07-04 21:37:31 -0500

    added search component

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,10 +1,25 @@
 var React = require('react');
 
 var Search = React.createClass({
+  handleSearch: function() {
+    var showCompleted = this.refs.showCompleted.checked;
+    var searchText = this.refs.searchText.value;
+
+    this.props.onSearch(showCompleted, searchText);
+  },
+
   render: function(){
       return(
       <div>
-        Search
+        <div>
+          <input type='text' ref='searchText' placeholder='Search Todo' onChange={this.handleSearch} />
+        </div>
+        <div>
+          <label>
+            <input type='checkbox' ref='showCompleted' onChange={this.handleSearch} />
+            Show completed todos
+          </label>
+        </div>
       </div>
     )
   }
