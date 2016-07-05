021bf35ef074d9707351bb4c4353913d5102e03e
Sang Yi <Sang Yi>
2016-07-05 08:00:48 -0500

    tested search component

--- app/tests/components/Search.test.jsx
+++ app/tests/components/Search.test.jsx
@@ -10,4 +10,28 @@ describe('Search', () => {
   it('should exist', () => {
     expect(Search).toExist();
   });
+
+  it('should call onSearch with entered input text', () => {
+    var searchText = 'Random';
+    var spy = expect.createSpy();
+    var search = TestUtils.renderIntoDocument(<Search onSearch={spy} />);
+
+    search.refs.searchText.value = searchText;
+    TestUtils.Simulate.change(search.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, 'Random');
+
+  });
+
+  it('should call onSearch with proper checked value', () => {
+
+    var spy = expect.createSpy();
+    var search = TestUtils.renderIntoDocument(<Search onSearch={spy} />);
+
+    search.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(search.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, '');
+
+  });
 });
